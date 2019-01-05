2cb07657d8e5261b50cef44466cb9693a3f28c7b
Stephen Done <srdone@gmail.com>
2019-01-04 21:19:33 -0700

    correct test again

--- tests/test.js
+++ tests/test.js
@@ -8,7 +8,7 @@ describe('Tests index', function() {
     index.get({ /* event */ }, { /* context */ }, (err, result) => {
       try {
         test.number(result.statusCode).is(200);
-        test.string(result.body).contains('Hello!');
+        test.string(result.body).contains('Hello');
         test.value(result).hasHeader('content-type', 'text/html');
         done();
       } catch(error) {
